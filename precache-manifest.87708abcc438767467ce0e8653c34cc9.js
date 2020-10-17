94f89e4ccc44b0f39213c11db27ac5253d191a3b
YanaD25 <yanaderevianko25@gmail.com>
2020-10-17 23:25:51 +0300

    Updates

---
+++ precache-manifest.87708abcc438767467ce0e8653c34cc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259644e00fd3c33bdb13640d1b58a419",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "260c564594979d523586",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a09a7b5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a09a7b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549015ef08ef68d36394",
+    "url": "/goit-react-hw-02-feedback/static/js/main.48c358de.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
